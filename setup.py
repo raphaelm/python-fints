d0172fb663fde05d60de6145e0971fe40ba64703
Raphael Michel <mail@raphaelmichel.de>
2017-01-07 18:45:11 +0100

    Packaging fix

--- setup.py
+++ setup.py
@@ -7,9 +7,12 @@ from fints import version
 
 here = path.abspath(path.dirname(__file__))
 
-# Get the long description from the relevant file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+try:
+    # Get the long description from the relevant file
+    with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+except:
+    long_description = ''
 
 setup(
     name='fints',
