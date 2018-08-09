f6984539a5f785711dd8ff16fbc1474472440e2a
Henryk Plötz <henryk@ploetzli.ch>
2018-08-10 01:30:03 +0200

    Experimental: print new parse for all incoming and outgoing messages

--- fints/connection.py
+++ fints/connection.py
@@ -4,6 +4,8 @@ import requests
 
 from .message import FinTSMessage
 
+from fints.parser import FinTS3Parser
+from fints.utils import Password
 
 class FinTSConnectionError(Exception):
     pass
@@ -14,9 +16,18 @@ class FinTSHTTPSConnection:
         self.url = url
 
     def send(self, msg: FinTSMessage):
+        print("Sending >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+        with Password.protect():
+            FinTS3Parser().parse_message(str(msg).encode('iso-8859-1')).print_nested()
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
         r = requests.post(
             self.url, data=base64.b64encode(str(msg).encode('iso-8859-1')),
         )
         if r.status_code < 200 or r.status_code > 299:
             raise FinTSConnectionError('Bad status code {}'.format(r.status_code))
-        return base64.b64decode(r.content.decode('iso-8859-1'))
+        retval = base64.b64decode(r.content.decode('iso-8859-1'))
+        print("Received <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+        with Password.protect():
+            FinTS3Parser().parse_message(retval).print_nested()
+        print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
+        return retval
