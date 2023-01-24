c71e75569f0a32482cd87466ad89c284a183ea98
yniverz <88168412+yniverz@users.noreply.github.com>
2023-01-24 19:39:22 +0000

    Update to "Segmentversion 2"

--- fints/segments/transfer.py
+++ fints/segments/transfer.py
@@ -12,13 +12,22 @@ class HKCCS1(FinTS3Segment):
     sepa_descriptor = DataElementField(type='an', max_length=256, _d="SEPA Descriptor")
     sepa_pain_message = DataElementField(type='bin', _d="SEPA pain message")
 
-class HKIPZ1(FinTS3Segment):
-    """SEPA-instant Einzelüberweisung, version 1
+# class HKIPZ1(FinTS3Segment):
+#     """SEPA-instant Einzelüberweisung, version 1
+#     
+#     Source: FinTS Financial Transaction Services, Schnittstellenspezifikation, Messages -- Multibankfähige Geschäftsvorfälle """
+#     account = DataElementGroupField(type=KTI1, _d="Kontoverbindung international")
+#     sepa_descriptor = DataElementField(type='an', max_length=256, _d="SEPA Descriptor")
+#     sepa_pain_message = DataElementField(type='bin', _d="SEPA pain message")
+
+class HKIPZ2(FinTS3Segment):
+    """SEPA-instant Einzelüberweisung, version 2
     
     Source: FinTS Financial Transaction Services, Schnittstellenspezifikation, Messages -- Multibankfähige Geschäftsvorfälle """
     account = DataElementGroupField(type=KTI1, _d="Kontoverbindung international")
     sepa_descriptor = DataElementField(type='an', max_length=256, _d="SEPA Descriptor")
     sepa_pain_message = DataElementField(type='bin', _d="SEPA pain message")
+    allow_convert_sepa_transfer = DataElementField(type="jn", _d="Allow conversion to SEPA transfer if instant-payment not supported")
 
 
 class HKCCM1(FinTS3Segment):
@@ -32,8 +41,18 @@ class HKCCM1(FinTS3Segment):
     sepa_pain_message = DataElementField(type='bin', _d="SEPA pain message")
 
     
-class HKIPM1(FinTS3Segment):
-    """SEPA-instant Sammelüberweisung, version 1
+# class HKIPM1(FinTS3Segment):
+#     """SEPA-instant Sammelüberweisung, version 1
+#     
+#     Source: FinTS Financial Transaction Services, Schnittstellenspezifikation, Messages -- Multibankfähige Geschäftsvorfälle """
+#     account = DataElementGroupField(type=KTI1, _d="Kontoverbindung international")
+#     sum_amount = DataElementGroupField(type=Amount1, _d="Summenfeld")
+#     request_single_booking = DataElementField(type='jn', _d="Einzelbuchung gewünscht")
+#     sepa_descriptor = DataElementField(type='an', max_length=256, _d="SEPA Descriptor")
+#     sepa_pain_message = DataElementField(type='bin', _d="SEPA pain message")
+
+class HKIPM2(FinTS3Segment):
+    """SEPA-instant Sammelüberweisung, version 2
     
     Source: FinTS Financial Transaction Services, Schnittstellenspezifikation, Messages -- Multibankfähige Geschäftsvorfälle """
     account = DataElementGroupField(type=KTI1, _d="Kontoverbindung international")
@@ -41,6 +60,7 @@ class HKIPM1(FinTS3Segment):
     request_single_booking = DataElementField(type='jn', _d="Einzelbuchung gewünscht")
     sepa_descriptor = DataElementField(type='an', max_length=256, _d="SEPA Descriptor")
     sepa_pain_message = DataElementField(type='bin', _d="SEPA pain message")
+    allow_convert_sepa_transfer = DataElementField(type="jn", _d="Allow conversion to SEPA transfer if instant-payment not supported")
 
 
 class HICCMS1(ParameterSegment):
