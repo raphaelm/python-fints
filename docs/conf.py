2bce6e8543d15d496b611910da4a8633d8ad71c8
Raphael Michel <michel@rami.io>
2024-10-04 15:55:06 +0200

    Try to fix rtd build

--- docs/conf.py
+++ docs/conf.py
@@ -386,4 +386,4 @@ epub_exclude_files = ['search.html']
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None, 'https://mt940.readthedocs.io/en/latest/': None, }
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None), 'mt940': ('https://mt940.readthedocs.io/en/latest/', None)}
